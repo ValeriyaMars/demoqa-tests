f29b390708ca8be0e783fb3791323b648564681f
valeriya.solovova <valeriya.solovova@simbirsoft.com>
2022-08-24 08:55:18 +0400

    added build.gradle, gitignore, removed extra spaces

--- src/test/java/ru/valeriamarshenina/tests/PracticeFormTests.java
+++ src/test/java/ru/valeriamarshenina/tests/PracticeFormTests.java
@@ -3,61 +3,57 @@ package ru.valeriamarshenina.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
+
+    String studentFirstName = "Valeriya",
+            studentLastName = "Mars";
+    String studentEmail = "test@test.ru";
+    String studentNumber = "9270280231";
+    String studentCurrentAddress = "Syzran";
+
     @BeforeAll
     static void beforeAll() {
         Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
-    void fillPracticeForm(){
-        String studentFirstName = "Valeriya";
-        String StudentLastName = "Mars";
-        String studentEmail = "test@test.ru";
-        String studentNumber = "9270280231";
-        String studentCurrentAddress = "Syzran";
+    void fillPracticeForm() {
 
-
-
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
         $("#firstName").setValue(studentFirstName);
-        $("#lastName").setValue(StudentLastName);
+        $("#lastName").setValue(studentLastName);
         $("#userEmail").setValue(studentEmail);
         $(byText("Female")).click();
         $("#userNumber").setValue(studentNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("7");
+        $(".react-datepicker__month-select").selectOptionByValue("8");
         $(".react-datepicker__year-select").selectOptionByValue("1988");
-        $(byText("28")).click();
+        $x("//div[contains(@aria-label, \"September 28th, 1988\")]").click();
         $("#subjectsInput").setValue("Maths").pressEnter();
         $(byText("Reading")).click();
-        $("input#uploadPicture").uploadFile(new File("C:/Users/Admin/Desktop/DSC04734.JPG"));
+        $("input#uploadPicture").uploadFromClasspath("img/1.png");
         $("#currentAddress").setValue(studentCurrentAddress);
         $("#state").click();
-        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
-
-        $(".modal-body").shouldHave(text("Student Name " + studentFirstName + " " + StudentLastName));
+        $(".modal-body").shouldHave(text("Student Name " + studentFirstName + " " + studentLastName));
         $(".modal-body").shouldHave(text("Student Email " + studentEmail));
         $(".modal-body").shouldHave(text("Gender Female"));
         $(".modal-body").shouldHave(text("Mobile " + studentNumber));
-        $(".modal-body").shouldHave(text("Date of Birth 28 August,1988"));
+        $(".modal-body").shouldHave(text("Date of Birth 28 September,1988"));
         $(".modal-body").shouldHave(text("Subjects Maths"));
         $(".modal-body").shouldHave(text("Hobbies Reading"));
-        $(".modal-body").shouldHave(text("Picture DSC04734.JPG"));
+        $(".modal-body").shouldHave(text("Picture 1.png"));
         $(".modal-body").shouldHave(text("Address " + studentCurrentAddress));
         $(".modal-body").shouldHave(text("State and City NCR Delhi"));
+
     }
 }
