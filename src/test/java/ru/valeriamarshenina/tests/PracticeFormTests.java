8c300344cae52c60614ec7b5b368818126355d01
valeriya.solovova <valeriya.solovova@simbirsoft.com>
2022-08-27 13:58:16 +0400

    added PageObjects and Java Faker in PracticeFormTests

--- src/test/java/ru/valeriamarshenina/tests/PracticeFormTests.java
+++ src/test/java/ru/valeriamarshenina/tests/PracticeFormTests.java
@@ -1,59 +1,49 @@
 package ru.valeriamarshenina.tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
+import ru.valeriamarshenina.pages.RegistrationPage;
+
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeFormTests {
+public class PracticeFormTests extends TestBase {
 
-    String studentFirstName = "Valeriya",
-            studentLastName = "Mars";
-    String studentEmail = "test@test.ru";
-    String studentNumber = "9270280231";
-    String studentCurrentAddress = "Syzran";
-
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.startMaximized = true;
-        Configuration.baseUrl = "https://demoqa.com";
-    }
+    RegistrationPage registrationPage = new RegistrationPage();
+    Faker faker = new Faker();
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            userNumber = faker.number().digits(10),
+            currentAddress = faker.address().fullAddress();
 
     @Test
     void fillPracticeForm() {
 
-        open("/automation-practice-form");
-        $("#firstName").setValue(studentFirstName);
-        $("#lastName").setValue(studentLastName);
-        $("#userEmail").setValue(studentEmail);
-        $(byText("Female")).click();
-        $("#userNumber").setValue(studentNumber);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("8");
-        $(".react-datepicker__year-select").selectOptionByValue("1988");
-        $x("//div[contains(@aria-label, \"September 28th, 1988\")]").click();
-        $("#subjectsInput").setValue("Maths").pressEnter();
-        $(byText("Reading")).click();
-        $("input#uploadPicture").uploadFromClasspath("img/1.png");
-        $("#currentAddress").setValue(studentCurrentAddress);
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $("#submit").click();
+        registrationPage.openPage();
 
-        $(".modal-body").shouldHave(text("Student Name " + studentFirstName + " " + studentLastName));
-        $(".modal-body").shouldHave(text("Student Email " + studentEmail));
-        $(".modal-body").shouldHave(text("Gender Female"));
-        $(".modal-body").shouldHave(text("Mobile " + studentNumber));
-        $(".modal-body").shouldHave(text("Date of Birth 28 September,1988"));
-        $(".modal-body").shouldHave(text("Subjects Maths"));
-        $(".modal-body").shouldHave(text("Hobbies Reading"));
-        $(".modal-body").shouldHave(text("Picture 1.png"));
-        $(".modal-body").shouldHave(text("Address " + studentCurrentAddress));
-        $(".modal-body").shouldHave(text("State and City NCR Delhi"));
+        registrationPage.typeFirstName(firstName)
+                .typeLastName(lastName)
+                .typeEmail(userEmail);
+        registrationPage.chooseGender();
+        registrationPage.typeNumber(userNumber);
+        registrationPage.calendar.setDate("7", "1988", "28");
+        registrationPage.chooseSubject("Maths").chooseSubject("Hindi");
+        registrationPage.chooseHobby();
+        registrationPage.uploadPicture("img/1.png");
+        registrationPage.typeCurrentAddress(currentAddress);
+        registrationPage.chooseState().chooseCity();
+        registrationPage.clickSubmitButton();
 
+        registrationPage.checkResultFormTitle();
+        registrationPage.checkResultsValue("Student Name", firstName + " " + lastName);
+        registrationPage.checkResultsValue("Student Email", userEmail);
+        registrationPage.checkResultsValue("Gender", "Female");
+        registrationPage.checkResultsValue("Mobile", userNumber);
+        registrationPage.checkResultsValue("Date of Birth", "28 August,1988");
+        registrationPage.checkResultsValue("Subjects", "Maths");
+        registrationPage.checkResultsValue("Hobbies", "Reading");
+        registrationPage.checkResultsValue("Picture", "1.png");
+        registrationPage.checkResultsValue("Address", currentAddress);
+        registrationPage.checkResultsValue("State and City", "NCR Delhi");
     }
 }
